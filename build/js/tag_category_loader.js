5e49f940cc0e0adcda99215710cbd2a42b54f5ba
Tac Tacelosky <tacman@gmail.com>
2021-10-08 13:32:20 -0400

    initial version

---
+++ build/js/tag_category_loader.js
@@ -0,0 +1,34 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["js/tag_category_loader"],{
+
+/***/ "./assets/js/Components/TagCategory.js":
+/*!*********************************************!*\
+  !*** ./assets/js/Components/TagCategory.js ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var TagCategory = function TagCategory() {
+  "use strict";
+
+  _classCallCheck(this, TagCategory);
+};
+
+module.exports = TagCategory;
+
+/***/ }),
+
+/***/ "./assets/js/tag_category_loader.js":
+/*!******************************************!*\
+  !*** ./assets/js/tag_category_loader.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var tagCategory = __webpack_require__(/*! ./Components/TagCategory */ "./assets/js/Components/TagCategory.js"); // now attach it to an element
+
+/***/ })
+
+},[["./assets/js/tag_category_loader.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvanMvQ29tcG9uZW50cy9UYWdDYXRlZ29yeS5qcyIsIndlYnBhY2s6Ly8vLi9hc3NldHMvanMvdGFnX2NhdGVnb3J5X2xvYWRlci5qcyJdLCJuYW1lcyI6WyJUYWdDYXRlZ29yeSIsIm1vZHVsZSIsImV4cG9ydHMiLCJ0YWdDYXRlZ29yeSIsInJlcXVpcmUiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0lBQU1BLFc7Ozs7OztBQUtOQyxNQUFNLENBQUNDLE9BQVAsR0FBaUJGLFdBQWpCLEM7Ozs7Ozs7Ozs7O0FDTEEsSUFBTUcsV0FBVyxHQUFHQyxtQkFBTyxDQUFDLHVFQUFELENBQTNCLEMsQ0FFQSw4QiIsImZpbGUiOiJqcy90YWdfY2F0ZWdvcnlfbG9hZGVyLmpzP2U1ODQ0OTFkYzA3NjRkZTg2YzM5Iiwic291cmNlc0NvbnRlbnQiOlsiY2xhc3MgVGFnQ2F0ZWdvcnkge1xuXG5cbn1cblxubW9kdWxlLmV4cG9ydHMgPSBUYWdDYXRlZ29yeTsiLCJjb25zdCB0YWdDYXRlZ29yeSA9IHJlcXVpcmUoJy4vQ29tcG9uZW50cy9UYWdDYXRlZ29yeScpO1xuXG4vLyBub3cgYXR0YWNoIGl0IHRvIGFuIGVsZW1lbnQiXSwic291cmNlUm9vdCI6IiJ9
